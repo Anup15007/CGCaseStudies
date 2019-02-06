17605c3fc69d86f2e35561fc38f92b6b4d4bca64
Anup15007 <47214732+Anup15007@users.noreply.github.com>
2019-02-06 10:18:15 +0530

    Add files via upload

--- CGPayrollSystem/src/com/cg/payroll/client/MainClass.java
+++ CGPayrollSystem/src/com/cg/payroll/client/MainClass.java
@@ -21,19 +21,46 @@ public class MainClass {
 				int choice = sc.nextInt();
 				switch (choice) {
 				case 1:
-					int associateId=services.acceptAssociateDetails("Anup", "Banerjee", "anup@gmail.com", "TCD", "A4", "CHD9776", 60000, 20000, 1000, 1500, 5896642, "Citi", "citi0006");
+					System.out.println("***********ENTER DETAILS***********\n");
+					System.out.println("Enter First name of associate: ");
+					String firstName = sc.next();
+					System.out.println("Enter Last name of associate: ");
+					String lastName = sc.next();
+					System.out.println("Enter Email Id of associate: ");
+					String emailId = sc.next();
+					System.out.println("Enter Department of associate: ");
+					String department = sc.next();
+					System.out.println("Enter Designation of associate: ");
+					String designation = sc.next();
+					System.out.println("Enter PAN number of associate:  ");
+					String pancard = sc.next();
+					System.out.println("Enter Yearly Investment: ");
+					int yearlyInvestmentUnder80C = sc.nextInt();
+					System.out.println("Enter Basic salary: ");
+					int basicSalary = sc.nextInt();
+					System.out.println("Enter EPF: ");
+					int epf = sc.nextInt();
+					System.out.println("Enter Company PF: ");
+					int companyPf = sc.nextInt();
+					System.out.println("Enter Account number: ");
+					int accountNumber = sc.nextInt();
+					System.out.println("Enter Bank name: ");
+					String bankName = sc.next();
+					System.out.println("Enter IFSC Code: ");
+					String ifscCode = sc.next();
+					int associateId=services.acceptAssociateDetails(firstName, lastName, emailId, department, designation, pancard, yearlyInvestmentUnder80C, basicSalary, epf, companyPf,accountNumber, bankName, ifscCode);
 					System.out.println("Associate Id: "+associateId);
 					break;
 				case 2:
 					System.out.println("Enter associate id to calculate net salary: ");
-					int assoId = sc.nextInt();
-					int netSalary = services.calculateNetSalary(assoId);
+					int id = sc.nextInt();
+					int netSalary = services.calculateNetSalary(id);
 					System.out.println("Net Salary = "+netSalary);
 					break;
 				case 3: 
 					System.out.println("Enter associate id to find details: ");
-					assoId = sc.nextInt();
-					Associate associateDetails = services.getAssociateDetails(assoId);
+					id = sc.nextInt();
+					Associate associateDetails = services.getAssociateDetails(id);
 					System.out.println(associateDetails);
 					break;
 				case 4:
@@ -41,7 +68,7 @@ public class MainClass {
 					System.out.println(allAssociateDetails);
 					break;
 			default:
-				System.out.println("Wrong Choice");
+				System.out.println("Please enter a valid number");
 				break;
 			}
 				System.out.println("Do you want to continue(y/n): ");
